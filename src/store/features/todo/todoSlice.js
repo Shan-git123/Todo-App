9c28aea6f4ee86aa921bb4ec7f06a63eb710f089
Shahin Qureshi <shahin2171993@gmail.com>
2023-06-20 11:36:55 +0530

    fixed error

--- src/store/features/todo/todoSlice.js
+++ src/store/features/todo/todoSlice.js
@@ -17,7 +17,7 @@ export const todoSlice = createSlice({
       state.todos = [];
     },
     deletedTodo: (state, action) => {
-      state.todos = state.todos.filter((todo) => todo.id != action.payload);
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
     },
     toggleForm: (state, action) => {
       state.toggleForm = !state.toggleForm;
